62709f328843a01f91470c728637145290671436
beel <nabeelmuhammad1209@gmail.com>
2024-08-20 15:57:56 +0700

    fix:fixing the back button calendly for demoDDP0

--- src/app/demoDDP0/page.tsx
+++ src/app/demoDDP0/page.tsx
@@ -1,14 +1,7 @@
 "use client"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
-
 export default function Home(){
-    const router = useRouter();
     return (
         <main className="relative w-screen h-screen bg-[#fbfcfd]">
-            <div className="z-10 bg-white p-2 absolute top-4 left-4 font-inter text-xl rounded-full font-semibold md:bg-[#0f0615] md:text-white cursor-pointer" onClick={()=>{router.push("../")}}>
-                &lt;--
-            </div>
             <div className="calendly-inline-widget calendly-mobile relative min-w-[320px] h-full m-auto" data-url="https://calendly.com/nabeel-muhammad-office/30min?background_color=0f0615&amp;text_color=ffffff&amp;primary_color=ffffff" data-processed="true">
                 <div className="calendly-spinner">
                     <div className="calendly-bounce1"></div>
